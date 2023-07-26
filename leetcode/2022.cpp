6472419cb54e0df4bf142083fd02e64655eba385
its-me-ojas <crestfallenwizard@gmail.com>
2023-07-26 12:37:26 +0530

    Convert 1D Array into 2D Array

---
+++ leetcode/2022.cpp
@@ -0,0 +1,59 @@
+// You are given a 0-indexed 1-dimensional (1D) integer array original, and two
+// integers, m and n. You are tasked with creating a 2-dimensional (2D) array
+// with  m rows and n columns using all the elements from original.
+
+// The elements from indices 0 to n - 1 (inclusive) of original should form the
+// first row of the constructed 2D array, the elements from indices n to 2 * n -
+// 1 (inclusive) should form the second row of the constructed 2D array, and so
+// on.
+
+// Return an m x n 2D array constructed according to the above procedure, or an
+// empty 2D array if it is impossible.
+
+//
+
+// Example 1:
+
+// Input: original = [1,2,3,4], m = 2, n = 2
+// Output: [[1,2],[3,4]]
+// Explanation: The constructed 2D array should contain 2 rows and 2 columns.
+// The first group of n=2 elements in original, [1,2], becomes the first row in
+// the constructed 2D array. The second group of n=2 elements in original,
+// [3,4], becomes the second row in the constructed 2D array. Example 2:
+
+// Input: original = [1,2,3], m = 1, n = 3
+// Output: [[1,2,3]]
+// Explanation: The constructed 2D array should contain 1 row and 3 columns.
+// Put all three elements in original into the first row of the constructed 2D
+// array. Example 3:
+
+// Input: original = [1,2], m = 1, n = 1
+// Output: []
+// Explanation: There are 2 elements in original.
+// It is impossible to fit 2 elements in a 1x1 2D array, so return an empty 2D
+// array.
+
+class Solution {
+public:
+  vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+    int originalSize = original.size();
+    // to check if its even possible to convert the original vector ito the 2d
+    // vector in given conditions
+    if (originalSize != (m * n))
+      return vector<vector<int>>();
+    vector<vector<int>> newVector;
+    int index = 0;
+    for (size_t i = 0; i < m; i++) {
+      // we use temp here to make the rows
+      vector<int> temp;
+      for (size_t j = 0; j < n; j++) {
+        // push the row wise elements into the temp vector
+        temp.push_back(original[index]);
+        index++;
+      }
+      // finally push the temp ( new row ) into the 2d vector
+      newVector.push_back(temp);
+    }
+    return newVector;
+  }
+};
