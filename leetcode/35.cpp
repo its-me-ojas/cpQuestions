be17bb7bb9549c228c70896a498b81556cdd48ff
its-me-ojas <crestfallenwizard@gmail.com>
2023-08-20 01:42:10 +0530

    Search Insert Postion

---
+++ leetcode/35.cpp
@@ -0,0 +1,41 @@
+
+// Given a sorted array of distinct integers and a target value, return the
+// index if the target is found. If not, return the index where it would be if
+// it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+//
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+// Approach : Binary Search
+
+class Solution {
+public:
+  int searchInsert(vector<int> &nums, int target) {
+    int start = 0, end = nums.size() - 1;
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      } else if (nums[mid] > target) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    return start;
+  }
+};
