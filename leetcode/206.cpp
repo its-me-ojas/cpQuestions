8fb1d372c0c70b300a9337936ec10e42c31d45c5
its-me-ojas <crestfallenwizard@gmail.com>
2023-09-08 20:20:47 +0530

    Reverse Linked List

--- leetcode/206.cpp
+++ leetcode/206.cpp
@@ -16,6 +16,7 @@
 
 // Input: head = []
 // Output: []
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -29,19 +30,31 @@
 class Solution {
 public:
   ListNode *reverseList(ListNode *head) {
+    // Check if the linked list is empty
     if (!head)
       return NULL;
 
+    // Initialize pointers for current, previous, and next nodes
     ListNode *curr = head;
     ListNode *prev = NULL;
 
+    // Iterate through the linked list
     while (curr->next != NULL) {
+      // Store the next node in a temporary variable
       ListNode *next = curr->next;
+
+      // Reverse the direction of the current node's pointer
       curr->next = prev;
+
+      // Move the prev and curr pointers one step forward
       prev = curr;
       curr = next;
     }
+
+    // Reverse the last node's pointer to the previous node
     curr->next = prev;
+
+    // Return the new head of the reversed linked list
     return curr;
   }
 };
