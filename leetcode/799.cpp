45dd76bfcc8b395e4f80a6103c54bc23a9c025df
its-me-ojas <crestfallenwizard@gmail.com>
2023-09-24 16:39:55 +0530

    Chanpagne Tower

---
+++ leetcode/799.cpp
@@ -0,0 +1,71 @@
+// We stack glasses in a pyramid, where the first row has 1 glass, the second
+// row has 2 glasses, and so on until the 100th row.  Each glass holds one cup
+// of champagne.
+
+// Then, some champagne is poured into the first glass at the top.  When the
+// topmost glass is full, any excess liquid poured will fall equally to the
+// glass immediately to the left and right of it.  When those glasses become
+// full, any excess champagne will fall equally to the left and right of those
+// glasses, and so on.  (A glass at the bottom row has its excess champagne fall
+// on the floor.)
+
+// For example, after one cup of champagne is poured, the top most glass is
+// full.  After two cups of champagne are poured, the two glasses on the second
+// row are half full.  After three cups of champagne are poured, those two cups
+// become full - there are 3 full glasses total now.  After four cups of
+// champagne are poured, the third row has the middle glass half full, and the
+// two outside glasses are a quarter full, as pictured below.
+
+// Now after pouring some non-negative integer cups of champagne, return how
+// full the jth glass in the ith row is (both i and j are 0-indexed.)
+
+//
+
+// Example 1:
+
+// Input: poured = 1, query_row = 1, query_glass = 1
+// Output: 0.00000
+// Explanation: We poured 1 cup of champange to the top glass of the tower
+// (which is indexed as (0, 0)). There will be no excess liquid so all the
+// glasses under the top glass will remain empty. Example 2:
+
+// Input: poured = 2, query_row = 1, query_glass = 1
+// Output: 0.50000
+// Explanation: We poured 2 cups of champange to the top glass of the tower
+// (which is indexed as (0, 0)). There is one cup of excess liquid. The glass
+// indexed as (1, 0) and the glass indexed as (1, 1) will share the excess
+// liquid equally, and each will get half cup of champange. Example 3:
+
+// Input: poured = 100000009, query_row = 33, query_glass = 17
+// Output: 1.00000
+
+class Solution {
+public:
+  double champagneTower(int poured, int query_row, int query_glass) {
+    // Create a 2D vector to represent the champagne tower with 'query_row + 1'
+    // rows. Each row has 'query_row + 1' glasses, initialized with 0.0
+    // champagne.
+    vector<vector<double>> tower(query_row + 1,
+                                 vector<double>(query_row + 1, 0.0));
+
+    // Pour the initial amount of champagne into the top glass.
+    tower[0][0] = static_cast<double>(poured);
+
+    // Iterate through each row of the tower.
+    for (int row = 0; row < query_row; ++row) {
+      // Iterate through each glass in the current row.
+      for (int glass = 0; glass <= row; ++glass) {
+        // Calculate the overflow of champagne from the current glass.
+        double overflow = (tower[row][glass] - 1.0) / 2.0;
+        if (overflow > 0) {
+          // Distribute the overflow evenly to the glasses in the next row.
+          tower[row + 1][glass] += overflow;
+          tower[row + 1][glass + 1] += overflow;
+        }
+      }
+    }
+
+    // Return the amount of champagne in the specified glass, capped at 1.0.
+    return min(1.0, tower[query_row][query_glass]);
+  }
+};
